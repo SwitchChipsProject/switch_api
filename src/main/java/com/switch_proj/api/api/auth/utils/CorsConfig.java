361b6f51636227207dfbd65274d852663a6829a2
minina0407 <minina@kyonggi.ac.kr>
2023-02-06 21:29:46 +0900

    [hotfix] CORS 설정 수정

--- src/main/java/com/switch_proj/api/api/auth/utils/CorsConfig.java
+++ src/main/java/com/switch_proj/api/api/auth/utils/CorsConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements  WebMvcConfigurer {
     @Bean
     public CorsFilter corsFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -20,5 +22,11 @@ public class CorsConfig {
         source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
     }
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
 }
+
